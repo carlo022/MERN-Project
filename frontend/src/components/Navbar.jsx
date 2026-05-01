3b3ae0bf4ebf42a24d2e447ea78e0aa68d169a59
carlo <carlolalas4@gmail.com>
2026-05-01 21:51:02 +0800

    ready for deployment

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,12 +1,14 @@
 import { Button, Container, Flex, HStack, Text, Icon } from "@chakra-ui/react";
 import { useColorMode } from "../components/ui/color-mode";
-import { Link } from "react-router-dom";
+import { Link, useLocation  } from "react-router-dom";
 // Changed LuPlusSquare to LuSquarePlus
 import { LuSquarePlus, LuSun } from "react-icons/lu"; 
 import { IoMoon } from "react-icons/io5";
+import { MdArrowBack, MdStore } from "react-icons/md";
 
 const Navbar = () => {
   const { colorMode, toggleColorMode } = useColorMode();
+  const location = useLocation();
 
   return (
     <Container 
@@ -26,6 +28,9 @@ const Navbar = () => {
           sm: "row",
         }}
       >
+        {location.pathname === "/CreateProduct" ?
+        (<Link to="/Homepage" style={{ display: "flex", alignItems: "center", gap: "8px" }}>
+          <Icon as={MdArrowBack} boxSize={8} color="purple.500"/>
         <Text
           fontSize={{ base: "2xl", sm: "3xl" }}
           fontWeight="bold"
@@ -33,21 +38,45 @@ const Navbar = () => {
           textAlign="center"
           // In v3, we can use the 'color' prop with a gradient shorthand
           bgGradient="to-r"
-          gradientFrom="cyan.400"
-          gradientTo="blue.500"
+          gradientFrom="purple.600"
+          gradientTo="pink.400"
           bgClip="text"
-        >
-          <Link to="/Homepage">Product Store 🛍️</Link>
-        </Text>
+          display="flex" 
+          alignItems="center" 
+          justifyContent="center"
+          gap={2}>
+            View Products
+          </Text>
+          </Link>)
+        :(<Link to="/Homepage" style={{ display: "flex", alignItems: "center", gap: "8px" }}>
+          <Text
+          fontSize={{ base: "2xl", sm: "3xl" }}
+          fontWeight="bold"
+          textTransform="uppercase"
+          textAlign="center"
+          // In v3, we can use the 'color' prop with a gradient shorthand
+          bgGradient="to-r"
+          gradientFrom="purple.600"
+          gradientTo="pink.400"
+          bgClip="text"
+          display="flex" 
+          alignItems="center" 
+          justifyContent="center"
+          gap={2}>
+            Product Store
+          </Text>
+          <Icon as={MdStore} boxSize={8} color="pink.400"/>
+          </Link>)
+          }
 
         {/* In v3, use 'gap' instead of 'spacing' */}
         <HStack gap={2} alignItems={"center"}>
           <Link to={"/CreateProduct"}>
             <Button 
               variant="solid"
-              bg="blue.500"
+              bg="purple.500"
               color="white"
-              _hover={{ bg: "blue.600" }}
+              _hover={{ bg: "purple.700" }}
             >
               <Icon asChild fontSize="2xl">
                 <LuSquarePlus />
